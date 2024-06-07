a8540e1711f1e3accd20997400fd3be2b82b59e6
Venkat <savipavan@gmail.com>
2024-06-08 00:01:27 +0530

    created_gitignore

---
+++ 1-rectangle_program.py
@@ -0,0 +1,12 @@
+"""
+Development steps:
+- Write down the objects on paper
+- Writing a class for each object
+- Writing methods for each class
+- Calling the classes and their methods
+"""
+
+
+class House:
+    def __init__(self, wall_area):
+        self.wall_area = wall_area
