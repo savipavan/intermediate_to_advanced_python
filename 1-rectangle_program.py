ef9a524113e6ff57d812e5a4cdea546ec44d8d6f
Venkat <savipavan@gmail.com>
2024-06-08 00:51:54 +0530

    created_gitignore

--- 1-rectangle_program.py
+++ 1-rectangle_program.py
@@ -7,6 +7,13 @@ Development steps:
 """
 
 
-class House:
-    def __init__(self, wall_area):
-        self.wall_area = wall_area
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
